bce5440169b3910e316f60dc0983908e219b8ea2
PokeMasterLEE <iyeojae@gmail.com>
2024-07-10 18:52:34 +0900

    feat/comment-entity : 댓글기능 엔티티

---
+++ src/main/java/com/sparta/springtrello/domain/comment/entity/Comment.java
@@ -0,0 +1,36 @@
+package com.sparta.springtrello.domain.comment.entity;
+
+import com.sparta.springtrello.common.Timestamped;
+import com.sparta.springtrello.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor
+@Table(name="comment")
+public class Comment extends Timestamped {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "card_id")
+//    private Card card;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Builder
+    public Comment(Card card, User user, String content) {
+        this.card = card;
+        this.user = user;
+        this.content = content;
+    }
+}
