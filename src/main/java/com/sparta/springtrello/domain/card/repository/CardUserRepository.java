219174d320008cbf861bb89b8b9315877299570b
semdev <tpal071997@gmail.com>
2024-07-15 20:05:24 +0900

    feat/card-member-delete : 카드에 초대된 멤버 제거

--- src/main/java/com/sparta/springtrello/domain/card/repository/CardUserRepository.java
+++ src/main/java/com/sparta/springtrello/domain/card/repository/CardUserRepository.java
@@ -3,5 +3,11 @@ package com.sparta.springtrello.domain.card.repository;
 import com.sparta.springtrello.domain.card.entity.CardUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
 public interface CardUserRepository extends JpaRepository<CardUser, Long> {
+
+    Optional<CardUser> findByCardIdAndUserId(Long cardId, Long userId);
+
 }
