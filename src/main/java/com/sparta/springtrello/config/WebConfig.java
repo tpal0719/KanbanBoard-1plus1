c7744268a2d0d276079d527910cf617cb3801b5f
dmsrb0125 <52693374+dmsrb0125@users.noreply.github.com>
2024-07-15 15:41:40 +0900

    refactor : 프론트 작업을 위한 수정

--- src/main/java/com/sparta/springtrello/config/WebConfig.java
+++ src/main/java/com/sparta/springtrello/config/WebConfig.java
@@ -5,6 +5,7 @@ import com.sparta.springtrello.common.OctetStreamReadMsgConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -21,5 +22,15 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
         converters.add(octetStreamReadMsgConverter);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000") // 프론트엔드 주소
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
     }
 }
